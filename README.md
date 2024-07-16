5037eb7e562dc8f72fa30e4028014ff2ef6bcac5
earlyBird <jayeshkrt@gmail.com>
2024-07-17 03:28:37 +0900

    17 july

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ All my code to prepare for SDE roles shall be kept and tracked here.
 
 16 July 2024
 Minimum window substring [https://leetcode.com/problems/minimum-window-substring/]
+
+17 July 2024
+Trapping rain water [https://leetcode.com/problems/trapping-rain-water/]
---
+++ trap_rain_water.py
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/trapping-rain-water/description/
+
+class Solution:
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        if not height: return 0
+
+        l, r = 0, len(height)-1
+
+        leftMax, rightMax = height[l], height[r]
+
+        res = 0
+
+        while l < r:
+            if leftMax < rightMax:
+                l += 1
+                leftMax = max(height[l], leftMax)
+                res += leftMax - height[l]
+            else:
+                r -= 1
+                rightMax = max(height[r], rightMax)
+                res += rightMax - height[r]
+        
+        return res
